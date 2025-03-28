65ca5e86fd208fb539088c47c0819b9b3467e7b8
PHILIP OBIORAH <philip.obiorah@uniport.edu.ng>
2025-03-28 20:24:04 +0000

    added convert cm to inches and feet

---
+++ convert_cm_to_inches_and_feet.py
@@ -0,0 +1,21 @@
+ # Height in Feet and Inches
+
+# Write a program that converts the user's height, given in centimeters, into feet and inches.
+# The height value is given in the variable height_cm. One inch is equal to 2.54 cm, and one foot is equal to 12 inches.
+# Your task is to calculate the number of complete feet in the given height and convert the remainder into inches.
+# Display the result on the screen.
+
+height_cm =  float(input("Enter user height in cm : "))
+print(height_cm)
+
+
+cm_per_inch = 2.54
+inch_per_foot = 12
+
+
+# calculate the number of complete feet in the given height cm
+
+# convert height cm to inches
+
+
+
