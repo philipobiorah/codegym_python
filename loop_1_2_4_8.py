fb18b2c4f17a1caac0ca5716a3308cb08b97bd0e
PHILIP OBIORAH <philip.obiorah@uniport.edu.ng>
2025-04-06 21:50:42 +0000

    added loop_1_2_4_8

---
+++ loop_1_2_4_8.py
@@ -0,0 +1,16 @@
+# The Clever Raja
+
+# Write a program that calculates how many grains of wheat will be on each square of a chessboard,
+# if one grain is placed on the first square, two grains on the second square, four grains on the third square, and so on.
+# There are a total of 64 squares on the chessboard.
+# Use a for loop and the range() function to iterate over the squares and the print() function to display the result.
+# Example:
+# For the first three squares, the program should output:
+# Square 1: 1 grain
+# Square 2: 2 grains
+# Square 3: 4 grains
+# ...
+
+# Write your code here
+for i in range(0, 63):
+    print(i)
