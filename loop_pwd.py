97bdf1b140c2e77fef32299308427e8cbe1d25e9
PHILIP OBIORAH <philip.obiorah@uniport.edu.ng>
2025-04-24 22:24:12 +0000

    added tenary.py

--- loop_pwd.py
+++ loop_pwd.py
@@ -14,3 +14,14 @@ else:
 for i in range(3):
     for j in range(3):
         print(f"{i},{j}")
+
+
+#multiplication table
+
+n = 5
+for i in range(1, n+1):
+    for j in range(1, n + 1):
+        print(f"{i} * {j} = {i*j}", end='\t')
+    #move to a new line
+    print()
+    
---
+++ tenary.py
@@ -0,0 +1,7 @@
+
+# Ternary Operator
+
+# Write a program that asks the user for a number and uses the ternary operator to check whether it is even or odd.
+# Display the appropriate message.
+
+# Write your code here
