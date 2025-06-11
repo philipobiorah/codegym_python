b26335f64f81b17013b41610cbdc894aa09be4ba
PHILIP OBIORAH <philip.obiorah@uniport.edu.ng>
2025-06-11 08:37:46 +0000

    added eight_grade_maths_fac

---
+++ eight_grade_Math_fac.py
@@ -0,0 +1,13 @@
+"""
+
+Write a program that asks the user for a number and uses the math library to calculate and display the square root and factorial of that number.
+
+Requirements:
+•	The program must include an import of the math library.
+•	The program must ask the user to input a number.
+•	The program must use the sqrt function from the math library to calculate the square root of the entered number.
+•	The program must use the factorial function from the math library to calculate the factorial of the entered number.
+•	The program should output the calculation results (the square root and factorial) to the screen.
+
+"""
+
