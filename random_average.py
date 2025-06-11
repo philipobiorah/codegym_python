da3a3a1eb7b26db291c1febc06e228deb161bf27
PHILIP OBIORAH <philip.obiorah@uniport.edu.ng>
2025-06-11 21:28:21 +0000

    completed random avg

--- random_average.py
+++ random_average.py
@@ -10,3 +10,24 @@ Requirements:
 •	The program must print the average value of the generated numbers on the screen
 """
 
+# Random Average
+
+# Write a program that generates 10 random numbers in the range from 1 to 100 using the random library.
+# Then calculate their average value and print it on the screen.
+
+# Write your code here
+import random
+
+
+
+ran_sum = 0
+#generate 10 random numbers 
+for i in range(10):
+    #in the range from 1 to 100
+    ran_sum += random.randint(1,100)
+
+#calculate their average
+ran_avg = ran_sum/10
+
+# print it on the screen
+print(ran_avg)
