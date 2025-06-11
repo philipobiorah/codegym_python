673345df41e0a97eaabb14fe717a5bacd234e99e
PHILIP OBIORAH <philip.obiorah@uniport.edu.ng>
2025-06-11 09:08:08 +0000

    added random_average.py

---
+++ random_average.py
@@ -0,0 +1,12 @@
+"""
+Write a program that generates 10 random numbers in the range from 1 to 100 using the random library.
+
+Then calculate their average value and print it on the screen.
+
+Requirements:
+•	The program must include the import of the random library.
+•	The program must use the function random.randint(1, 100) to generate each of the 10 random numbers.
+•	The program must calculate the average value of the generated numbers by summing all the numbers and dividing the result by their count (10).
+•	The program must print the average value of the generated numbers on the screen
+"""
+
