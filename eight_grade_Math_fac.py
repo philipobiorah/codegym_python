6e731bab9259c7cbde2e283bb58b0e6c6f914b86
PHILIP OBIORAH <philip.obiorah@uniport.edu.ng>
2025-06-21 09:40:25 +0000

    addes eight_grade_math_fac

--- eight_grade_Math_fac.py
+++ eight_grade_Math_fac.py
@@ -11,3 +11,26 @@ Requirements:
 
 """
 
+import math
+
+#ask the user to input a number.
+
+num = int(input("Enter a number : "))
+
+
+#use the sqrt function from the math library to calculate the square root of the entered number.
+num_square = math.sqrt(num)
+
+#calulate the factorial of the number
+# 5 factorial is 5! = 5 x 4 x 3 x 2 x 1 = 120
+# 0 factorial is a definition: 0! = 1. There is exactly 1 way to arrange 0 objects
+
+num_fac = 1
+for i in range(1, num + 1):
+    num_fac *= (num + 1 - i)
+
+print(f'Factorial: {num_fac}')
+
+
+#using the math fucntion math.factorial(x)
+# num_fac = math.factorial(num)
